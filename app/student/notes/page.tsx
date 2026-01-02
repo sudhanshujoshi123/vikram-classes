f73bcd11438c351a66bf0885e6c637bda9c4b381
Sudhanshu Joshi <Sudhanshu@nainigroup.com>
2026-01-02 21:50:14 +0530

    build fixed

--- app/student/notes/page.tsx
+++ app/student/notes/page.tsx
@@ -1,8 +1,7 @@
 export default function StudentNotesPage() {
   return (
     <div style={{ padding: 40 }}>
-      <h1>Student Notes Page</h1>
-      <p>Temporary page to fix build error</p>
+      <h1>Notes section coming soon</h1>
     </div>
   );
 }
