29a4f89e6cdeab34f97abc453960b5b04354b25b
Sudhanshu Joshi <Sudhanshu@nainigroup.com>
2026-01-02 20:47:18 +0530

    ui

---
+++ app/student/notes/page.tsx
@@ -0,0 +1,8 @@
+export default function StudentNotesPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Student Notes Page</h1>
+      <p>Temporary page to fix build error</p>
+    </div>
+  );
+}
