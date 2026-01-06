7fda31c64f5d045d9897dde461e77883527166d0
Sudhanshu Joshi <Sudhanshu@nainigroup.com>
2026-01-06 15:20:20 +0530

    st

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,18 +5,26 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://vikramclasses.com',
       lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
     },
     {
       url: 'https://vikramclasses.com/login',
       lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
     },
     {
       url: 'https://vikramclasses.com/register',
       lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
     },
     {
       url: 'https://vikramclasses.com/student',
       lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
     },
   ];
 }
