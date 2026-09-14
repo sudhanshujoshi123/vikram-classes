2e014ff3940c72d72bd452fa38abd44f7cbe6215
sudhanshujoshi123 <sudhansh24r@gmail.com>
2026-09-15 00:01:52 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -701,7 +701,7 @@ function Contact() {
           />
           <ContactCard
             role="System Architect & Developer"
-            name="Mr.Sudhanhsu Joshi [BTKIT Dwarahat]"
+            name="Mr. Sudhanhsu Joshi [BTKIT Dwarahat]"
             phone="7900431779"
             emails={["sudhanshu24r@gmail.com"]}
             gradient="from-indigo-500/10 to-violet-500/10"
